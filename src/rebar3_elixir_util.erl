9051bbcff7be0946ef128c36b8aeb090bb173476
Sushruth Sivaramakrishnan <sivsushruth@gmail.com>
2016-08-14 21:47:28 +0530

    Discover elixir and mix and add to path

--- src/rebar3_elixir_util.erl
+++ src/rebar3_elixir_util.erl
@@ -2,6 +2,7 @@
 
 -export([add_elixir/1, get_details/1, add_states/4, compile_libs/1, clean_app/2, transfer_libs/3, to_binary/1]).
 
+-spec to_binary(binary()|list()|integer()|atom()) -> binary().
 to_binary(V) when is_binary(V) -> V;
 to_binary(V) when is_list(V) -> list_to_binary(V);
 to_binary(V) when is_integer(V) -> integer_to_binary(V);
@@ -11,23 +12,40 @@ to_binary(_) -> erlang:error(badarg).
 add_elixir(State) ->
     {BinDir, Env, Config, LibDir} = rebar3_elixir_util:get_details(State),
     MixState = rebar3_elixir_util:add_states(State, BinDir, Env, Config),
-    code:add_patha(LibDir ++ "/elixir/ebin"),
-    code:add_patha(LibDir ++ "/mix/ebin"),
+    code:add_patha(filename:join(LibDir, "elixir/ebin")),
+    code:add_patha(filename:join(LibDir, "mix/ebin")),
     MixState.
 
 get_details(State) ->
     Config = rebar_state:get(State, elixir_opts),
-    {lib_dir, LibDir} = lists:keyfind(lib_dir, 1, Config),
-    {bin_dir, BinDir} = lists:keyfind(bin_dir, 1, Config),
+    BinDir = case lists:keyfind(bin_dir, 1, Config) of
+                false -> 
+                    {ok, ElixirBin_} = find_executable("elixir"),
+                    filename:dirname(ElixirBin_);
+                {bin_dir, Dir1} -> Dir1
+             end, 
+
+    LibDir = case lists:keyfind(lib_dir, 1, Config) of
+                false -> 
+                    {ok, ElixirLibs_} = rebar_utils:sh("elixir -e 'IO.puts :code.lib_dir(:elixir)'", []),
+                    filename:join(re:replace(ElixirLibs_, "\\s+", "", [global,{return,list}]), "../");
+                {lib_dir, Dir2} -> Dir2
+             end,            
     {env, Env} = lists:keyfind(env, 1, Config),
     {BinDir, Env, Config, LibDir}.
+
+find_executable(Name) ->
+    case os:find_executable(Name) of
+        false -> false;
+        Path -> {ok, filename:nativename(Path)}
+    end.
 
 add_states(State, BinDir, Env, Config) ->
     EnvState = rebar_state:set(State, mix_env, Env),
     RebarState = rebar_state:set(EnvState, elixir_opts, Config),
     BaseDirState = rebar_state:set(RebarState, elixir_base_dir, filename:join(rebar_dir:root_dir(RebarState), "elixir_libs/")),
-    ElixirState = rebar_state:set(BaseDirState, elixir, BinDir ++ "/elixir "),
-    rebar_state:set(ElixirState, mix, BinDir ++ "/mix ").    
+    ElixirState = rebar_state:set(BaseDirState, elixir, filename:join(BinDir, "elixir ")),
+    rebar_state:set(ElixirState, mix, filename:join(BinDir, "mix ")).    
 
 compile_libs(State) ->
     {ok, Apps} = rebar_utils:list_dir(rebar_state:get(State, elixir_base_dir)),
