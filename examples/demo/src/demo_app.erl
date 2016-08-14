1a173f13df6b96f0b965bca071954a2e4117e055
Sushruth Sivaramakrishnan <sivsushruth@gmail.com>
2016-08-15 00:15:03 +0530

    add decimal_demo_run

--- examples/demo/src/demo_app.erl
+++ examples/demo/src/demo_app.erl
@@ -8,7 +8,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2, stop/1, decimal_demo_run/0]).
 
 %%====================================================================
 %% API
@@ -16,6 +16,12 @@
 
 start(_StartType, _StartArgs) ->
     demo_sup:start_link().
+
+decimal_demo_run() ->
+    A = 'Elixir.Decimal':new(7),
+    B = 'Elixir.Decimal':new(4),
+    C = 'Elixir.Decimal':add(A, B),
+    io:format("Result is : ~p", [C]).
 
 %%--------------------------------------------------------------------
 stop(_State) ->
